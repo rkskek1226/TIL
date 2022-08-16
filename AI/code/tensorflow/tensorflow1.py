4542f2087f6b9602fbf7ac400a4436578f8ecaf9
rkskek1226 <hkcho01@naver.com>
2022-08-16 23:05:43 +0900

    2022/08/16

--- AI/code/tensorflow/tensorflow1.py
+++ AI/code/tensorflow/tensorflow1.py
@@ -3,6 +3,18 @@ import numpy as np
 
 np.set_printoptions(precision=3)   # 소수점 4번째 자리에서 반올림
 
+rank_0_tensor = tf.constant(4)   # 스칼라(rank-0)
+print(rank_0_tensor)
+
+rank_1_tensor = tf.constant([2.0, 3.0, 4.0])   # 벡터(rank-2)
+print(rank_1_tensor)
+
+rank_3_tensor1 = tf.constant([[1, 2], [3, 4], [5, 6]], dtype=tf.float16)   # 행렬(rank-3)
+print(rank_3_tensor1)
+
+rank_3_tensor2 = tf.constant([[[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]], [[10, 11, 12, 13, 14], [15, 16, 17, 18, 19]], [[20, 21, 22, 23, 24], [25, 26, 27, 28, 29]]])
+print(rank_3_tensor2)
+
 a = np.array([1, 2, 3])
 b = [4, 5, 6]
 
@@ -42,4 +54,41 @@ t = tf.zeros((1, 2, 1, 4, 1))
 t_sqz = tf.squeeze(t, axis=(2, 4))   # 차원 삭제(크기가 1인 차원은 불필요하므로 삭제)
 print(t.shape)
 print(t_sqz.shape)
+
+c = tf.constant([[40., 5.], [10., 1.], [11., 6]])
+print(c)
+print(tf.reduce_max(c))   # largest value
+print(tf.argmax(c, axis=1))   # index of largest value
+print(tf.nn.softmax(c))
+
+
+# 비정형 텐서
+# 특정 축에서 다양한 수의 요소를 가진 텐서
+# tf.ragged.RaggedTensor를 사용
+ragged_tensor = tf.ragged.constant([[0, 1, 2, 3], [4, 5], [6, 7, 8], [9]])
+print(ragged_tensor)
+print(ragged_tensor.shape)
+print()
+
+
+# 문자열 텐서
+# tf.string을 사용
+string_tensor1 = tf.constant("Hell world")
+print(string_tensor1)
+string_tensor2 = tf.constant(["Hell world", "HIHI", "BYEBYE"])
+print(string_tensor2)
+print(tf.strings.split(string_tensor1, sep=" "))
+print(tf.strings.split(string_tensor2))
+
+
+# 희소 텐서
+# tf.sparse.SparseTensor를 사용
+# indices는 0이 아닌 값이 있는 위치, values는 값
+sparse_tensor = tf.sparse.SparseTensor(indices=[[0, 0], [1, 2]], values=[10, 20], dense_shape=[3, 4])
+print(sparse_tensor)
+print()
+print(tf.sparse.to_dense(sparse_tensor))
+
+
+
 
